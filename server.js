2aa55f5617c07ec78cdf52c2998a95a7cfadffe1
Matthew Chan <chanchan4219@ucla.edu>
2015-05-21 01:54:54 -0700

    Initialize files

---
+++ server.js
@@ -0,0 +1,64 @@
+var multer = require("multer")
+var express = require("express")
+var app = express()
+var path = require("path")
+var fs = require("fs")
+var uploadDone = false
+var fileName = ""
+
+// Configure multer
+app.use(multer({
+	// Define the destination for uploaded files
+	dest: "./uploads/",
+	// Choose a unique naming system for uploaded files
+	rename: function(fieldname, filename) {
+		fileName = filename + Date.now()
+		return fileName
+	},
+	// File upload event triggers
+	onFileUploadStart: function(file) {
+		console.log(file.originalname + " is uploading...")
+	},
+	onFileUploadComplete: function(file) {
+		console.log(file.originalname + " uploaded to " + file.path)
+		uploadDone = true
+	}
+}))
+
+// Set Express JS view engine
+app.set("views", __dirname + "/public")
+app.set("view engine", "ejs")
+
+// Set up Express JS static file directory
+app.use(express.static(__dirname + "/public"))
+
+/************* Specify GET and POST request/response handling ***************/
+
+// Main page loads with file upload .ejs form
+app.get("/", function(request, response) {
+	response.render("form.ejs")
+})
+
+// Show page contains the uploaded image file
+app.get("/show", function(request, response) {
+	var filePath = path.join(__dirname + "/uploads/" + fileName +".jpg")
+	response.writeHead(200, {"Content-Type":"image/jpg"})
+	fs.createReadStream(filePath).pipe(response)
+})
+
+// When form is submitted, redirect user to Upload page. Page displays the uploaded image file
+app.post("/upload", function(request, response) {
+	var html = "File successfully uploaded."
+	html += "<br/><img src='/show' alt=''/>"
+	if (uploadDone) {
+		console.log(request.files)
+		response.writeHead(200, {"Content-Type":"text/html"})
+		response.write(html)
+		response.end()
+	}
+})
+
+// Go to localhost:8888 to view the website
+app.listen(8888, function() {
+	console.log("Server started.")
+})
