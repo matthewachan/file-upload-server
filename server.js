4e4d8aec8dc008a0c5648e9a7f6e8e91355e2990
Matthew Chan <matthew.a.chan@gmail.com>
2015-05-22 16:08:06 -0700

    Update comments for clarity

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ var express = require("express")
 var app = express()
 var path = require("path")
 var fs = require("fs")
-var uploadDone = false
-var fileName = ""
 
-// Configure multer
+var uploadDone = false
+var uploadName = ""
+
+// Configure multer object
 app.use(multer({
 	// Define the destination for uploaded files
 	dest: "./uploads/",
 	// Choose a unique naming system for uploaded files
 	rename: function(fieldname, filename) {
-		fileName = filename + Date.now()
-		return fileName
+		uploadName = filename + Date.now()
+		return uploadName
 	},
 	// File upload event triggers
 	onFileUploadStart: function(file) {
@@ -28,6 +29,7 @@ app.use(multer({
 // Set Express JS view engine
 app.set("views", __dirname + "/public")
 app.set("view engine", "ejs")
+
 // Set the default port
 app.set("port", (process.env.PORT || 5000))
 
@@ -41,14 +43,14 @@ app.get("/", function(request, response) {
 	response.render("form.ejs")
 })
 
-// Show page contains the uploaded image file
+// This page contains the uploaded image file
 app.get("/show", function(request, response) {
-	var filePath = path.join(__dirname + "/uploads/" + fileName +".jpg")
+	var filePath = path.join(__dirname + "/uploads/" + uploadName +".jpg")
 	response.writeHead(200, {"Content-Type":"image/jpg"})
 	fs.createReadStream(filePath).pipe(response)
 })
 
-// When form is submitted, redirect user to Upload page. Page displays the uploaded image file
+// When form is submitted, redirect user to a new page displaying the uploaded image file
 app.post("/upload", function(request, response) {
 	var html = "File successfully uploaded."
 	html += "<br/><img src='/show' alt=''/>"
@@ -60,7 +62,7 @@ app.post("/upload", function(request, response) {
 	}
 })
 
-// Go to localhost:5000 to view the website
+// Server listens on the default port
 app.listen(app.get('port'), function() {
 	console.log("Server started on port ", app.get("port"))
 })
